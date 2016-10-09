67bef440577d417cd2cd1e03297b298ae254fec5
Duncan Marsh <dwolfm@gmail.com>
2016-10-09 00:41:08 -0700

    tested fuzzy-query

---
+++ test/fuzzy-query-test.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const expect = require('chai').expect
+const fuzzyQuery = require('../lib/find-fuzzy-query-gen.js')
+
+describe('testing module find-fuzzy-query-gen', function(){
+  describe('with valid input', function(){
+    let fields = ['name', 'duck']
+    let query = {
+      name: 'slug',
+      duck: 'quack',
+    }
+
+    it('should return a mongo query', done => {
+      let result = fuzzyQuery(fields, query)
+      console.log('result', result)
+      expect(result.name['$regex'].toString()).to.equal('/.*s.*l.*u.*g.*/')
+      expect(result.duck.$regex.toString()).to.equal('/.*q.*u.*a.*c.*k.*/')
+      done()
+    })
+  })
+
+  describe('with bad array', function(){
+    let fields = '' 
+    let query = {
+      name: 'slug',
+      duck: 'quack',
+    }
+
+    it('should return a mongo query', done => {
+      let result = fuzzyQuery(fields, query)
+      expect(JSON.stringify(result)).to.equal('{}')
+      done()
+    })
+  })
+
+  describe('with bad array', function(){
+    let fields = []
+    let query = ''
+
+    it('should return a mongo query', done => {
+      let result = fuzzyQuery(fields, query)
+      expect(JSON.stringify(result)).to.equal('{}')
+      done()
+    })
+  })
+})
